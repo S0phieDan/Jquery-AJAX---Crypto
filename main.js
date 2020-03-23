ec9d2963ab5d7b0560246ca377a9f38cc56916d8
Sophie <danilovsophie@gmail.com>
2020-03-23 14:21:39 +0200

    added modal

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function createCardForCoin(coin,i)
 {
+    let coinsFromLS = JSON.parse(localStorage.getItem('coinsToRepsLocal'));
+
     let divCard = $('<div></div>');
-    divCard.addClass('card border-primary mb-3');
+    divCard.addClass('card border-primary md-3');
     divCard.attr("style", "width: 20rem;");
     
 
@@ -12,10 +14,22 @@ function createCardForCoin(coin,i)
     divHeadText.html("<h3>" + coin.symbol + "</h3>");
     divHeaderCard.append(divHeadText);
 
+    
     let toggle = $('<div class="custom-control custom-switch"></div>');
     toggle.attr("style", "float: right;");
     let toggleInput = $('<input type="checkbox" class="custom-control-input">');
     toggleInput.attr("id",'toggle'+ i+'');
+    if(coinsFromLS.indexOf(toggleInput.attr("id")) === -1)
+    {
+        toggleInput.attr("is_checked","false");
+
+    }
+    else{
+        toggleInput.attr("is_checked","true");
+        toggleInput.attr('checked','');
+
+    }
+    toggleInput.attr("onclick", 'addCoinToReps('+'"'+ toggleInput.attr("id")+'"'+')');
     toggle.append(toggleInput);
     let toggleLabel = $('<label class="custom-control-label"></label>');
     toggleLabel.attr("for",'toggle'+ i+'');
@@ -95,6 +109,52 @@ function openMoreInfo(index,string_id)
 
 }
 
+function createModal()
+{
+    let modal = $('<div class="modal show"></div>');
+    let modal_dialog = $('<div class="modal-dialog" role="document"></div>');
+    let modal_content =$('<div class="modal-content"></div>');
+    let modal_header = $('<div class="modal-header"><h5 class="modal-title">Modal title</h5><button class="close" aria-label="Close" type="button" data-dismiss="modal"> <span aria-hidden="true">&times;</span></button></div>');
+    let modal_body =$('<div class="modal-body"><p>Modal body text goes here.</p></div>');
+    let modal_footer =$('<div class="modal-footer"><button class="btn btn-primary" type="button">Save changes</button><button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button></div>');
+
+    modal_content.append(modal_header);
+    modal_content.append(modal_body);
+    modal_content.append(modal_footer);
+
+    modal_dialog.append(modal_content);
+    modal.append(modal_dialog);
+
+    $('.container').append(modal);
+
+}
+
+function addCoinToReps(id){
+    let coinsFromLS = JSON.parse(localStorage.getItem('coinsToRepsLocal'));
+    let state = $('#'+id+'').attr("is_checked");
+    let result = state.localeCompare("false");
+
+    if(result === 0){
+        $('#'+id+'').attr("is_checked", "true");
+        if(coinsFromLS.length <= 4){
+            coinsFromLS.push(id);
+        }
+        else{
+            createModal();
+            
+        }
+        
+        
+    }
+    else{
+        $('#'+id+'').attr("is_checked", "false");
+        coinsFromLS.splice(coinsFromLS.indexOf(id),1);
+    }
+    
+    localStorage.setItem('coinsToRepsLocal', JSON.stringify(coinsFromLS));
+
+}
+
 
 $(document).ready(function(){
 
@@ -108,9 +168,17 @@ $(document).ready(function(){
         $('.loading').attr("style", "display: none;");
         var coins = data;
 
+        if(localStorage.getItem('coinsToRepsLocal') == null)
+        {
+            let coinsToReps = [];
+            localStorage.setItem('coinsToRepsLocal', JSON.stringify(coinsToReps));
+        }
+        
+
         /*coins.forEach(coin => {
             createCardForCoin(coin);
         });*/
+
 
         for(let i=0; i<1000; i++){
             createCardForCoin(coins[i],i);
